58b7814e0d5e4a3e9980a5060f7cf15ba6eee476
RyMcQueen <116460235+RyMcQueen@users.noreply.github.com>
2022-10-24 15:13:50 -0600

    v21

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+
+const magnify = <FontAwesomeIcon icon={faMagnifyingGlass}/> 
+const profileIcon = <FontAwesomeIcon icon={faUser}/>
+
  
 const Menu = () => {
     return (
@@ -13,13 +20,13 @@ const Menu = () => {
                     <div><Link to="/NFL" className='League_Route'>NFL</Link></div>
                 </div>
 
-                <div><input id='Search_Bar' type='text' placeholder='  Search..'></input></div>
+                <div className='Search_Div'>{magnify}<input id='Search_Bar' type='text' placeholder='  Search..'></input></div>
 
                 <div>
-                    <div><Link to='/AccountCreation' className='Account_Route' >Account</Link></div>
+                    <div><Link to='/AccountCreation' className='Account_Route' >{profileIcon} Account</Link></div>
                 </div>
 
-            </div>
+            </div> 
             <div className='Bottom_Nav'>
                 <div><Link to="/" id='Home_Header'>Sports Inclusive</Link></div>
                 <div><button className='SignIn' id='SignIn'><Link className='SignIn' to='Signin'>Sign In</Link></button></div>
