6947e37c915e95d3ab19701fd745f6987d6baa66
EvanMacLeanPK <106777883+EvanMacLeanPK@users.noreply.github.com>
2022-10-26 17:18:34 -0600

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,25 +6,66 @@ const home = () => {
           <h1 id='Title'>Top Stories</h1>
             <div id='Inlayed'>
                 <div>
-                    <h3>Artificial Title Here</h3>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <br/>
-                    <p>Tollere odium autem in nostra potestate sint, ab omnibus et contra naturam transferre in nobis. 
-                    Sed interim toto desiderio supprimunt: si vis aliqua quae in manu tua tibi necesse confundentur et quae, 
-                    quod laudabile esset, nihil tamen possides.
-                    Oportet uti solum de actibus prosequtionem et fugam, haec leniter et blandus et reservato.
-                    Quae tibi placent quicunq prosunt aut diligebat multum, quod memor sis ad communia sunt ab initio minima. Quod si,
-                    exempli gratia, cupidum rerum in propria sunt ceramic calicem, admonere te solum Ceramic, quod sit generalis poculis, 
-                    in cuius es tu cupidum. Deinde, si
-                    </p>
+                <h4  id = "NHL-Title">Weekly Sports Highlights</h4>
+                <div id = "NHL-Left-News">
+
+                </div>
+                <div id = "NHL-Left-Text">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet massa vitae tortor condimentum lacinia. Vitae elementum curabitur vitae nunc. Non tellus orci ac auctor augue. Eu consequat ac felis donec et odio pellentesque diam. Pharetra et ultrices neque ornare aenean.</p>
+                </div>
+                <div id = "NHL-Tag-Box">
+                    <p id = "NHL-Tag">#NBA</p>
+                    <p id = "NHL-Tag">#NHL</p>
+                    <p id = "NHL-Tag">#NFL</p>
+                </div>
+                <h5 id = "NHL-Bottom-Text">See More NHL</h5>
                 </div>
 
                 <div id='HomePageRight'>
+                    <div id = "Artical">
 
+                        <div id = "News-Artical">
+                            <div id = "Artical-Image"></div>
+                            <h5 id = "Artical-Title">New NHL, NFL and NBA app sweeps the nation.</h5>
+                            <div id = "New-Tag-Box">
+                                <p id = "New-Tag">#NBA</p>
+                                <p id = "New-Tag">#NHL</p>
+                                <p id = "New-Tag">#NFL</p>
+                            </div>
+                        </div>
+
+                        <div id = "News-Artical-Bottom">
+                            <div id = "Artical-Image"></div>
+                            <h5 id = "Artical-Title">Calgary Flames off to a amazing start to the 2022/23 regular seasson</h5>
+                            <div id = "New-Tag-Box">
+                                <p id = "New-Tag">#NHL</p>
+                                <p id = "New-Tag">#Calgary Flames</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div id = "Artical1">
+
+                        <div id = "News-Artical">
+                            <div id = "Artical-Image"></div>
+                            <h5 id = "Artical-Title">New NHL, NFL and NBA app sweeps the nation</h5>
+                            <div id = "New-Tag-Box">
+                                <p id = "New-Tag">#NBA</p>
+                                <p id = "New-Tag">#NHL</p>
+                                <p id = "New-Tag">#NFL</p>
+                            </div>
+                        </div>
+
+                        <div id = "News-Artical-Bottom">
+                            <div id = "Artical-Image"></div>
+                            <h5 id = "Artical-Title">Calgary Flames off to a amazing start to the 2022/23 regular seasson</h5>
+                            <div id = "New-Tag-Box">
+                                <p id = "New-Tag">#NHL</p>
+                                <p id = "New-Tag">#Calgary Flames</p>
+                            </div>
+                        </div>
+                        </div>
+                    
                 </div>
 
             </div>
@@ -32,4 +73,5 @@ const home = () => {
     );
 }
  
+export default home;
 export default home;
